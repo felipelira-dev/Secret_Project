cea0a7ba1c3e6398e6423aba62bfb9ba7685f5aa
Felipe Lira <163548758+felipelira-dev@users.noreply.github.com>
2025-08-27 19:44:45 -0300

    Install bcrypt

--- Scritp/Script.py
+++ Scritp/Script.py
@@ -49,7 +49,7 @@ def login(usuario, senha):
 
 
 # --- Exemplo de uso ---
-if _name_ == "_main_":
+if "_name_" == "_main_":
     # Cadastrando alguns usuários
     cadastrar_usuario("felipe", "minhaSenhaSuperForte")
     cadastrar_usuario("maria", "senhaDaMaria123")
